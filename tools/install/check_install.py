ce84794dbddc68b3545462520b05da9c323a9034
applenob <applecer@pku.edu.cn>
2019-07-29 14:25:55 +0800

    fix ci bug

--- tools/install/check_install.py
+++ tools/install/check_install.py
@@ -11,7 +11,6 @@ import sys
 
 # you should add the libraries which are not included in setup.py
 MANUALLY_INSTALLED_LIBRARIES = [
-    ('espnet', None),
     ('kaldiio', None),
     ('matplotlib', None),
     ('librosa', None),
