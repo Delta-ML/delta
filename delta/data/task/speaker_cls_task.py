670cb161e1217a3856ab4a5a0a1e7618648708c2
chenjunwen <chenjunwen@didiglobal.com>
2019-08-05 13:45:00 +0800

    support asr, emotion, speaker

--- delta/data/task/speaker_cls_task.py
+++ delta/data/task/speaker_cls_task.py
@@ -36,7 +36,7 @@ from delta.data.task.base_speech_task import SpeechTask
 class ChunkSampler():
   ''' Produce samples from utterance. '''
 
-  def __init__(self, chunk_size):
+  def __init__(self, meta, chunk_size):
     '''
       Args:
       chunk_size: fixed chunk size in frames.
@@ -44,14 +44,32 @@ class ChunkSampler():
     '''
     self.chunk_size = chunk_size
     self.pad_chunks = True
+    self.add_random_offset = False
+    self.drop_short_chunks = 0.0
+    self.single_chunk = False
 
-  def set_chunk_size(self, chunk_size):
-    ''' Set chunk size. '''
-    self.chunk_size = chunk_size
+    self.meta = meta
+    self.spk_keys = None
+    self.spk_keys_pointer = 0
+    self.utt_keys = None
+    self.meta_keys_pointer = 0
+    self.select_by_spk = True
+    self.num_repeats = 1
 
-  def set_pad_chunks(self, pad_chunks):
-    ''' Set whether to zero pad chunks. '''
-    self.pad_chunks = pad_chunks
+  def copy(self):
+    ''' Return a copy of self, except meta data. '''
+    new = ChunkSampler(None, self.chunk_size)
+    new.pad_chunks = self.pad_chunks
+    new.add_random_offset = self.add_random_offset
+    new.drop_short_chunks = self.drop_short_chunks
+    new.single_chunk = self.single_chunk
+    new.select_by_spk = self.select_by_spk
+    return new
+
+  def get_bare_sampler(self):
+    ''' Return a new sampler with no meta data. '''
+    new = self.copy()
+    return new.utt_to_samples
 
   def utt_to_samples(self, args):
     '''
@@ -95,11 +113,25 @@ class ChunkSampler():
 
     results = []
     chunk_idx = 0
-    for offset in range(0, num_frames_voiced, self.chunk_size):
+    if self.add_random_offset:
+      random_offset = np.random.randint(0, self.chunk_size)
+    else:
+      random_offset = 0
+    for offset in range(random_offset, num_frames_voiced, self.chunk_size):
+      if self.single_chunk:
+        available = num_frames_voiced - self.chunk_size
+        if available < 0:
+          # No padding.
+          logging.warn('Single chunk mode: available < 0.')
+          break
+        offset = random.randint(0, available)
       logging.debug('offset = %d' % (offset))
       feat_chunk = feat_mat_voiced[offset:offset + self.chunk_size, :]
       unpadded_frames = feat_chunk.shape[0]
       if self.pad_chunks and unpadded_frames < self.chunk_size:
+        rel_chunk_len = float(unpadded_frames) / self.chunk_size
+        if rel_chunk_len < self.drop_short_chunks:
+          continue
         logging.debug('Padding chunk of frames %d ...' % (unpadded_frames))
         padded = np.zeros((self.chunk_size, feat_dim), dtype=feat_chunk.dtype)
         padded[:unpadded_frames, :] = feat_chunk
@@ -109,6 +141,8 @@ class ChunkSampler():
       sample = (feat_chunk, spk_id, sample_key)
       chunk_idx += 1
       results.append(sample)
+      if self.single_chunk:
+        break
     if result_queue:
       # queue mode
       result_queue.put(results)
@@ -116,6 +150,52 @@ class ChunkSampler():
     # imap mode
     return results
 
+  def reset(self):
+    ''' Reset progress. '''
+    if self.select_by_spk:
+      logging.info('Shuffling spk keys ...')
+      self.spk_keys = []
+      for index in range(self.num_repeats):
+        temp_keys = list(self.meta.spks.keys())
+        random.shuffle(temp_keys)
+        self.spk_keys.extend(temp_keys)
+    else:
+      logging.info('Shuffling utt keys ...')
+      self.utt_keys = []
+      for index in range(self.num_repeats):
+        temp_keys = list(self.meta.utts.keys())
+        random.shuffle(temp_keys)
+        self.utt_keys.extend(temp_keys)
+
+  def next_part_utts(self, part_size):
+    ''' Return next part of utts. '''
+    if self.select_by_spk:
+      # Select utterances by speaker.
+      for index in range(part_size):
+        try:
+          spk = self.spk_keys.pop()
+        except IndexError:
+          break
+        spk_utts = self.meta.spks[spk].utts
+        if not spk_utts:
+          # None (e.g. with external id mapping) or len == 0
+          continue
+        random_idx = random.randint(0, len(spk_utts) - 1)
+        if random_idx < 0:
+          # Not likely.
+          continue
+        utt_key = spk_utts[random_idx]
+        utt_meta = self.meta.utts[utt_key]
+        yield (utt_key, utt_meta)
+    else:
+      # Randomly selects utterances.
+      for index in range(part_size):
+        try:
+          utt_key = self.utt_keys.pop()
+        except IndexError:
+          break
+        utt_meta = self.meta.utts[utt_key]
+        yield (utt_key, utt_meta)
 
 class DataQueueAsync():
   ''' Sample from raw data. '''
@@ -159,7 +239,7 @@ class ManagerQueueDataQueue(DataQueueAsync):
     self.pool = mp.Pool(self.num_processes)
     pool_args = [(self.mp_queue, x) for x in self.meta.utts.items()]
     self.pool_res = self.pool.map_async(
-        self.sampler.utt_to_samples, pool_args, chunksize=100)
+        self.sampler.get_bare_sampler(), pool_args, chunksize=100)
     self.pool.close()
 
   def done(self):
@@ -180,10 +260,7 @@ class ImapUnorderedDataQueue(DataQueueAsync):
 
   def __init__(self, meta, sampler, num_processes=None, max_qsize=20):
     super().__init__(meta, sampler, num_processes, max_qsize)
-    self.meta = meta
-    self.meta_keys = []
-    self.meta_keys_pointer = 0
-    self.end_of_inputs = False
+    self.meta = meta  # TODO: remove
     self.sampler = sampler
     self.pool = None
     self.pool_chunk_size = 1000
@@ -193,35 +270,30 @@ class ImapUnorderedDataQueue(DataQueueAsync):
   def start(self):
     ''' Start sampling async. '''
     self.pool = mp.Pool(self.num_processes)
-    self.meta_keys = list(self.meta.utts.keys())
-    logging.info('Shuffling utt keys ...')
-    random.shuffle(self.meta_keys)
-
-  def next_chunk(self):
-    ''' Return an iterator of next chunk of utterances. '''
-    for index in range(self.meta_keys_pointer,
-                       self.meta_keys_pointer + self.pool_chunk_size):
-      if index < len(self.meta_keys):
-        utt_key = self.meta_keys[index]
-        utt_meta = self.meta.utts[utt_key]
-        yield (utt_key, utt_meta)
-      else:
-        self.end_of_inputs = True
-    self.meta_keys_pointer = index + 1
 
   def append_requests(self):
-    ''' Append some requests to pool. '''
-    pool_args = [(None, x) for x in self.next_chunk()]
+    '''
+    Append some requests to pool.
+
+    Returns:
+      a bool, True if still has unconsumed data, False otherwise.
+    '''
+    pool_args = [(None, x) for x in
+                 self.sampler.next_part_utts(self.pool_chunk_size)]
     self.pool_res = self.pool.imap_unordered(
-        self.sampler.utt_to_samples, pool_args, chunksize=100)
+        self.sampler.get_bare_sampler(), pool_args, chunksize=100)
+    if pool_args:
+      return True
+    else:
+      return False
 
   def get_items(self):
     ''' Get a generator of results. '''
     while True:
-      self.append_requests()
+      has_remaining = self.append_requests()
       for item in self.pool_res:
         yield item
-      if self.end_of_inputs:
+      if not has_remaining:
         break
     self.pool.close()
 
@@ -240,14 +312,54 @@ class SpeakerClsTask(SpeechTask):
 
     self.data_type = self.taskconf['data_type']
 
+    if 'whole_utt_inference' in self.taskconf['audio']:
+      self.whole_utt_inference = self.taskconf['audio']['whole_utt_inference']
+    else:
+      self.whole_utt_inference = False
+
+    if 'add_random_offset' in self.taskconf['audio']:
+      self.add_random_offset = self.taskconf['audio']['add_random_offset']
+    else:
+      self.add_random_offset = False
+
+    if 'drop_short_chunks' in self.taskconf['audio']:
+      self.drop_short_chunks = self.taskconf['audio']['drop_short_chunks']
+    else:
+      self.drop_short_chunks = 0.0
+
+    if 'single_chunk' in self.taskconf['audio']:
+      self.single_chunk = self.taskconf['audio']['single_chunk']
+    else:
+      self.single_chunk = 0.0
+
+    if 'select_by_spk' in self.taskconf['audio']:
+      self.select_by_spk = self.taskconf['audio']['select_by_spk']
+    else:
+      self.select_by_spk = 0.0
+
+    if 'num_repeats' in self.taskconf['audio']:
+      self.num_repeats = self.taskconf['audio']['num_repeats']
+    else:
+      self.num_repeats = 1
+
     self.chunk_size_seconds = self.taskconf['audio']['clip_size']
     # TODO: configurable frame rate
     self.chunk_size_frames = self.chunk_size_seconds * 100
     self.feature_dims = self.taskconf['audio']['feature_size']
     # TODO: delta features
-    self.feature_shape = (self.chunk_size_frames, self.feature_dims, 1)
+    if self.mode == utils.INFER and self.whole_utt_inference:
+      self.feature_shape = (None, self.feature_dims, 1)
+    else:
+      self.feature_shape = (self.chunk_size_frames, self.feature_dims, 1)
+    self.feature_shape = (None, self.feature_dims, 1)
     # TODO: text input is useless
     self.max_text_len = 10
+
+    # TODO: not implemented
+    self.uniform_resample = False
+
+    # 10k sample/utts is somehow enough.
+    self.gen_cmvn_max_samples = 10000
 
     self._cmvn_path = self.taskconf['audio']['cmvn_path']
 
@@ -258,7 +370,32 @@ class SpeakerClsTask(SpeechTask):
     logging.info('Loading meta data ...')
     self.load_meta_data()
 
-    self.sampler = ChunkSampler(self.chunk_size_frames)
+    if self.mode == utils.INFER and self.whole_utt_inference:
+      # Do whole utterance inference.
+      logging.info('Set chunk_size = 10M and padding = False.')
+      self.sampler = ChunkSampler(self.meta, 10000000)
+      self.sampler.pad_chunks = False
+    else:
+      self.sampler = ChunkSampler(self.meta, self.chunk_size_frames)
+
+    if self.mode != utils.INFER and self.add_random_offset:
+      self.sampler.add_random_offset = True
+
+    if self.mode != utils.INFER and self.drop_short_chunks > 0.0:
+      logging.info('Dropping chunks < %f .' % (self.drop_short_chunks))
+      self.sampler.drop_short_chunks = self.drop_short_chunks
+
+    if self.mode != utils.INFER:
+      logging.info('Single chunk sampling enabled.')
+      self.sampler.single_chunk = self.single_chunk
+
+    if self.mode != utils.INFER:
+      logging.info('Utt selection by spk enabled.')
+      self.sampler.select_by_spk = self.select_by_spk
+
+    if self.mode == utils.TRAIN:
+      logging.info('Num repeats = %d.' % (self.num_repeats))
+      self.sampler.num_repeats = self.num_repeats
 
   def load_meta_data(self):
     ''' Load meta data. '''
@@ -297,8 +434,10 @@ class SpeakerClsTask(SpeechTask):
         self.taskconf['audio']['feature_size'],
         self.taskconf['audio']['add_delta_deltas'])
 
-    self.sampler.set_chunk_size(100000)
-    self.sampler.set_pad_chunks(False)
+    self.sampler.chunk_size = 100000
+    self.sampler.pad_chunks = False
+
+    num_done = 0
     for inputs, _, _, _, _, _ in \
         self.generate_data():
       # update stats
@@ -306,6 +445,11 @@ class SpeakerClsTask(SpeechTask):
         inputs = np.expand_dims(inputs, axis=0)
       sums, square, count = utils.update_cmvn_statis(
           inputs, sums, square, count, axis=(0, 1))
+      num_done += 1
+      if num_done % 100 == 0:
+        logging.info('Done %d samples.' % (num_done))
+      if num_done > self.gen_cmvn_max_samples:
+        break
     # compute cmvn
     mean, var = utils.compute_cmvn(sums, square, count)
     if dry_run:
@@ -353,6 +497,8 @@ class SpeakerClsTask(SpeechTask):
     else:
       multiprocess = True
 
+    self.sampler.reset()
+
     if multiprocess:
       q = ImapUnorderedDataQueue
       data_queue = q(self.meta, self.sampler, num_processes=4)
@@ -397,16 +543,19 @@ class SpeakerClsTask(SpeechTask):
         output_shapes=shapes,
     )
 
-    if 'tf_shuffle_buffer_size' in self.solverconf['optimizer']:
-      buffer_size = self.solverconf['optimizer']['tf_shuffle_buffer_size']
-    else:
-      buffer_size = 100000
+    buffer_size = self.taskconf['shuffle_buffer_size']
     logging.info('Using buffer size of %d samples in shuffle_and_repeat().' %
                  (buffer_size))
     if mode == utils.TRAIN:
-      data = data.apply(
-          tf.data.experimental.shuffle_and_repeat(
-              buffer_size=buffer_size, count=1, seed=None))
+      data = data.shuffle(buffer_size=buffer_size)
+      if self.uniform_resample:
+        def class_func(inputs, texts, labels, filenames, clip_ids, soft_labels):
+          ''' Return the label of a sample tuple. '''
+          return labels
+        target_dist = tf.ones((self.num_class,), dtype=tf.float32) / \
+                      self.num_class
+        data = data.apply(tf.data.experimental.rejection_resample(
+                          class_func, target_dist))
 
     def make_example(inputs, texts, labels, filenames, clip_ids, soft_labels):
       features = {
@@ -419,6 +568,10 @@ class SpeakerClsTask(SpeechTask):
       }
       return features, labels
 
+    if self.mode == utils.INFER and self.whole_utt_inference:
+      # To avoid length difference since padding = False.
+      logging.info('Inference mode, set batch_size to 1.')
+      batch_size = 1
     return data.map(make_example, num_parallel_calls=10).\
                 batch(batch_size, drop_remainder=False).\
                 prefetch(tf.contrib.data.AUTOTUNE)
