c232dcaf66adefbc406d2d4a4c4fbbdae4d53089
guotingwei <guotingwei@didiglobal.com>
2019-08-06 17:41:52 +0800

    update config

--- egs/hkust/asr/v1/run_delta.sh
+++ egs/hkust/asr/v1/run_delta.sh
@@ -4,6 +4,6 @@ set -x
 
 . path.sh
 
-CUDA_VISIBLE_DEVICES=2,3 python3 -u $MAIN_ROOT/delta/main.py --config conf/asr-ctc.yml --cmd train
+CUDA_VISIBLE_DEVICES=2 python3 -u $MAIN_ROOT/delta/main.py --config conf/asr-ctc.yml --cmd train_and_eval
 
 
