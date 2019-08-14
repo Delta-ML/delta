73b5fef13c20f6507bb73e00541d7c6d37b7113e
Yiping Peng <pengyiping@didichuxing.com>
2019-08-14 20:42:06 +0800

    Support logits_type of linear, linear_no_bias, arcface.

--- delta/models/speaker_cls_rawmodel.py
+++ delta/models/speaker_cls_rawmodel.py
@@ -16,6 +16,7 @@
 '''
 A series of models for speaker classification.
 '''
+import math
 from absl import logging
 import tensorflow as tf
 import tensorflow.keras.layers as keras_layers
@@ -223,18 +224,82 @@ class SpeakerBaseRawModel(RawModel):
         hidden_idx += 1
     return embedding, y
 
-  def logits_layer(self, x):
+  def arcface_layer(self, inputs, labels, output_num, weights):
+    ''' ArcFace layer. '''
+
+    def arcface_loss(embedding, labels, out_num, weights, s=64., m=0.5):
+      '''
+      https://github.com/auroua/InsightFace_TF/blob/master/losses/face_losses.py
+      :param embedding: the input embedding vectors
+      :param labels:  the input labels, the shape should be eg: (batch_size, 1)
+      :param s: scalar value default is 64
+      :param out_num: output class num
+      :param m: the margin value, default is 0.5
+      :return: the final cacualted output, this output is send into the tf.nn.softmax directly
+      '''
+      cos_m = math.cos(m)
+      sin_m = math.sin(m)
+      mm = sin_m * m  # issue 1
+      threshold = math.cos(math.pi - m)
+      with tf.variable_scope('arcface_loss'):
+        # inputs and weights norm
+        embedding_norm = tf.norm(embedding, axis=1, keep_dims=True)
+        embedding = tf.div(embedding, embedding_norm, name='norm_embedding')
+        weights_norm = tf.norm(weights, axis=0, keep_dims=True)
+        weights = tf.div(weights, weights_norm, name='norm_weights')
+        # cos(theta+m)
+        cos_t = tf.matmul(embedding, weights, name='cos_t')
+        cos_t2 = tf.square(cos_t, name='cos_2')
+        sin_t2 = tf.subtract(1., cos_t2, name='sin_2')
+        sin_t = tf.sqrt(sin_t2, name='sin_t')
+        cos_mt = s * tf.subtract(
+            tf.multiply(cos_t, cos_m), tf.multiply(sin_t, sin_m), name='cos_mt')
+
+        # this condition controls the theta+m should in range [0, pi]
+        #      0<=theta+m<=pi
+        #     -m<=theta<=pi-m
+        cond_v = cos_t - threshold
+        cond = tf.cast(tf.nn.relu(cond_v, name='if_else'), dtype=tf.bool)
+
+        keep_val = s * (cos_t - mm)
+        cos_mt_temp = tf.where(cond, cos_mt, keep_val)
+
+        mask = tf.one_hot(labels, depth=out_num, name='one_hot_mask')
+        # mask = tf.squeeze(mask, 1)
+        inv_mask = tf.subtract(1., mask, name='inverse_mask')
+
+        s_cos_t = tf.multiply(s, cos_t, name='scalar_cos_t')
+
+        output = tf.add(
+            tf.multiply(s_cos_t, inv_mask),
+            tf.multiply(cos_mt_temp, mask),
+            name='arcface_loss_output')
+      return output
+    params = self.netconf['arcface_params']
+    s = params['s']
+    m = params['m']
+    return arcface_loss(inputs, labels, output_num, weights, s=s, m=m)
+
+  def logits_layer(self, x, labels):
     ''' Logits layer to further produce softmax. '''
-    if self.netconf['has_logits_layer']:
-      with tf.variable_scope('logits'):
-        logits = common_layers.linear(
-            x, 'logits-matmul',
-            [x.shape[-1].value, self.taskconf['classes']['num']])
-      return logits
-    else:
-      logging.info('has_logits_layer == False, not adding logits layer.')
-      logging.info('Make sure to use a loss with internal logits layer.')
-      return x
+    output_num = self.taskconf['classes']['num']
+    logits_type = self.netconf['logits_type']
+    logits_shape = [x.shape[-1].value, output_num]
+    with tf.variable_scope('logits'):
+      weights = tf.get_variable(
+        name='weights',
+        shape=logits_shape,
+        initializer=tf.truncated_normal_initializer(stddev=0.1))
+      if logits_type == 'linear':
+        bias = tf.get_variable(
+          name='bias',
+          shape=logits_shape[1],
+          initializer=tf.constant_initializer(0.0))
+        return tf.matmul(x, weights) + bias
+      elif logits_type == 'linear_no_bias':
+        return tf.matmul(x, weights)
+      elif logits_type == 'arcface':
+        return self.arcface_layer(x, labels, output_num, weights)
 
 
 @registers.model.register
@@ -248,7 +313,7 @@ class SpeakerCRNNRawModel(SpeakerBaseRawModel):
     x = self.lstm_layer(x)
     x = self.pooling_layer(x)
     embedding, dense_output = self.dense_layer(x)
-    logits = self.logits_layer(dense_output)
+    logits = self.logits_layer(dense_output, labels)
     model_outputs = {'logits': logits, 'embeddings': embedding}
     return model_outputs
 
@@ -313,7 +378,7 @@ class SpeakerTDNNRawModel(SpeakerBaseRawModel):
     x, _ = self.tdnn_block(feats)
     x = self.pooling_layer(x)
     embedding, dense_output = self.dense_layer(x)
-    logits = self.logits_layer(dense_output)
+    logits = self.logits_layer(dense_output, labels)
     model_outputs = {'logits': logits, 'embeddings': embedding}
     return model_outputs
 
@@ -387,7 +452,7 @@ class SpeakerResNetRawModel(SpeakerBaseRawModel):
     x = self.linear_block(x)
     x = self.pooling_layer(x)
     embedding, dense_output = self.dense_layer(x)
-    logits = self.logits_layer(dense_output)
+    logits = self.logits_layer(dense_output, labels)
     model_outputs = {'logits': logits, 'embeddings': embedding}
     return model_outputs
 
