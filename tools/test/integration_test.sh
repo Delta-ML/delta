67cb24c9bef174c49acec68726c35a88bd96942c
Hui Zhang <zhtclz@foxmail.com>
2019-08-26 20:29:36 +0800

    fix test

--- tools/test/integration_test.sh
+++ tools/test/integration_test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Set bash to 'debug' mode, it will exit on :
-# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
-set -e
-set -u
-set -o pipefail
-
-
-
-if [ -z $MAIN_ROOT ];then
+if [ -z ${MAIN_ROOT} ];then
   if [ -f env.sh ];then 
       source env.sh
   else
@@ -16,10 +8,15 @@ if [ -z $MAIN_ROOT ];then
   fi
 fi
 
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
 
 echo "Integration Testing..."
 
-pushd $MAIN_ROOT/egs/mini_an4/asr/v1 
+pushd ${MAIN_ROOT}/egs/mini_an4/asr/v1
 bash run_delta.sh || echo "mini an4 error" && exit 1
 popd
 
