07056db4711926c99dfa751f6c7255244b1fd2ea
GaryGao <53735637+GaryGao99@users.noreply.github.com>
2019-09-25 13:36:53 +0800

    add base frontend (#84)
    * add base frontend
    
    * fix doc
    
    * fix syntax error
    
    * rename frontend and hparam
    
    * add fbank
    
    * fix bug
    
    * add read wav
    
    * fix read_wav
    
    * specific interfaces of feature functions
    
    * fix fbank
    
    * Update read_wav.py
    
    add return sample_rate
    
    * Update read_wav_test.py
    
    fix test_wav
    
    * spectrum
    
    add spectrum and its test
    
    * Update speech_features.png
    
    fix fbank op
    
    * fbank
    
    fix fbank and it's test
    
    * change test_wav
    
    * fix rely
    
    * fix config setting
    
    * pitch
    
    * fix config setting
    
    * pitch
    
    * cepstrum
    
    * fix pitch
    
    * fix call
    
    * fix config and call
    
    * zcr
    
    * plp
    
    * framepow
    
    * analyfiltbank, synthfiltbank, delta_delta
    
    * fix spectrum
    
    * fix comment
    
    * fix fbank test
    
    * fix fbank
    
    * fix delta_delta
    
    * fix fbank and it's test
    
    * fix hparam
    
    * fix sample_rate setting
    
    * fbank_pitch features
    
    * add deepdiff package
    
    * fix fbank_pitch features
    
    * get fbank_pitch features using kaldi io
    
    * make fbank_pitch features using shell
    
    * fix sample_rate setting and add doc
    
    * fix dtype of params
    
    * fix sample_rate && add doc
    
    * fix feat_lib dependency and sample rate
    
    * remove contrib_audio dependency
    
    * Delete compute_pitch_feats.py
    
    * Merge branch 'master' into frontend
    
    * fix espnet path
    
    * delete make_fbank_pitch.sh
    
    * fix sample rate and doc
    
    * fix doc
    
    * Revert "remove contrib_audio dependency"
    
    This reverts commit a7735e84001b44dcc3170bd080e43d2841cb7506.
    
    * fix read_wav
    
    * fix import error
    
    * fix read && write wav
    
    * fix assert sample rate
    
    * make fbank features
    
    * make features
    
    * fix assert sample rate
    
    * fix write wav
    
    * fix write_wav return
    
    * fix write_wav test
    
    * Update write_wav_test.py

---
+++ delta/data/frontend/read_wav_test.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2017 Beijing Didi Infinity Technology and Development Co.,Ltd.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import tensorflow as tf
+import os
+from pathlib import Path
+from delta.data.frontend.read_wav import ReadWav
+import librosa
+
+class ReadWavTest(tf.test.TestCase):
+
+  def test_read_wav(self):
+    wav_path = str(
+      Path(os.environ['MAIN_ROOT']).joinpath('delta/layers/ops/data/sm1_cln.wav'))
+
+    with self.session():
+      read_wav = ReadWav.params({'sample_rate':16000.0}).instantiate()
+      audio_data, sample_rate = read_wav(wav_path)
+      audio_data_true, sample_rate_true = librosa.load(wav_path, sr=16000)
+      self.assertAllClose(audio_data.eval(), audio_data_true)
+      self.assertAllClose(sample_rate.eval(), sample_rate_true)
+
+if __name__ == '__main__':
+  tf.test.main()
