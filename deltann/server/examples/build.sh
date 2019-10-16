ee8201da5fff82df5e0f7664136d2ca4c6eab6dd
itamaker <itamaker@gmail.com>
2019-10-16 16:05:20 +0800

    *fix build process & add readme

--- deltann/server/examples/build.sh
+++ deltann/server/examples/build.sh
@@ -1,19 +1,27 @@
-#!/bin/sh
+#! /bin/bash
+set -euf -o pipefail
 delta_file="delta-service"
 if [ -f "$delta_file" ]; then
 rm -rf $delta_file
 fi
-
 GOOS=linux GOARCH=amd64 go build -o $delta_file  main.go
 if [ -d output ]
 then
     rm -rf output
 fi
 mkdir -p output/$delta_file/log
-cp -R  ../dpl output
+#cp -R  ../dpl output
+mkdir -p output/dpl/output/include
+cp -R  ../../../dpl/lib/ output/dpl/output
+pushd output/dpl/output/lib/custom_ops
+mv x_ops.so libx_ops.so
+popd
+cp -R  ../../../dpl/model/ output/dpl/output
+cp -R ../../api/c_api.h output/dpl/output/include
 cp $delta_file output/$delta_file
 cp control.sh output/$delta_file
 cp run.sh output/$delta_file
-cp env.sh output/$delta_file
+cp server-env.sh output/$delta_file
+
 
 
