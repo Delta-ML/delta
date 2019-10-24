67c77dc86fbef119ee6b37ae19889187f824dce0
Hui Zhang <zhtclz@foxmail.com>
2019-10-24 13:12:17 +0800

    fix go server example

--- deltann/server/examples/build.sh
+++ deltann/server/examples/build.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 set -euf -o pipefail
 
+source server-env.sh
+
 delta_file="delta-service"
 
 if [ -f "$delta_file" ]; then
-rm -rf $delta_file
+  rm -rf $delta_file
 fi
 
-if [ -d output ]
-then
+if [ -d output ]; then
     rm -rf output
 fi
 
 mkdir -p output/$delta_file/log
 mkdir -p output/dpl/
 
-cp -R ../../../dpl/output/ output/dpl/
+cp -R $MAIN_ROOT/dpl/output/ output/dpl/
 
 cp -R ../mock_yaml/nlp/model.yaml   output/dpl/output/model/saved_model/1/
 
@@ -26,11 +27,11 @@ fi
 
 cp -R output/dpl ../
 
+# build delta serving
 GOOS=linux GOARCH=amd64 go build -o $delta_file  main.go
 
+# copy utils
 cp $delta_file output/$delta_file
-cp control.sh output/$delta_file
 cp run.sh output/$delta_file
 cp server-env.sh output/$delta_file
-
-
+chmod +x output/$delta_file/run.sh
