9eac2ce2598c60862c323663d241dad66cc6837b
dengchengyun <dengchengyun@didiglobal.com>
2019-11-11 18:32:45 +0800

    Merge branch 'ci' of https://github.com/didi/delta into ci

--- tools/install/prepare_kaldi.sh
+++ tools/install/prepare_kaldi.sh
@@ -24,13 +24,20 @@ if [ -z ${MAIN_ROOT} ];then
   fi
 fi
 
+if [ `id -u` == 0 ];then
+  SUDO=sudo
+else
+  SUDO=
+fi
+
 if ! [ -d ${MAIN_ROOT}/tools/kaldi ];then
   pushd ${MAIN_ROOT}/tools && git clone --depth=1 https://github.com/kaldi-asr/kaldi.git && popd
 fi
 
+
 pushd ${MAIN_ROOT}/tools/kaldi/tools
 #sudo apt-get install zlib1g-dev wget subversion
-extras/check_dependencies.sh
+extras/check_dependencies.sh || ${SUDO} apt-get install -y zlib1g-dev wget gfortran subversion
 
 SPH2PIPE_VERSION=v2.5
 wget -T 10 -t 3 https://www.openslr.org/resources/3/sph2pipe_${SPH2PIPE_VERSION}.tar.gz || wget -T 10 https://sourceforge.net/projects/kaldi/files/sph2pipe_${SPH2PIPE_VERSION}.tar.gz || exit 1
