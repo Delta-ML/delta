751e16aefd77447ff2e9926730437fa9c96f4ada
Hui Zhang <zhtclz@foxmail.com>
2019-11-11 15:41:31 +0800

    fix kaldi tools install script

--- tools/test/integration_test.sh
+++ tools/test/integration_test.sh
@@ -16,7 +16,7 @@ set -o pipefail
 
 #prepare kaldi
 if [ ! -d ${MAIN_ROOT}/tools/kaldi/tools/sph2pipe_v2.5 ]; then
-  bash ${MAIN_ROOT}/tools/test/prepare_kaldi.sh
+  bash ${MAIN_ROOT}/tools/install/prepare_kaldi.sh
 fi
 
 echo "Integration Testing..."
