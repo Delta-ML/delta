247761784d42e4537d04f1566c54cd4cee9c8055
Hui Zhang <zhtclz@foxmail.com>
2019-11-14 19:59:45 +0800

    fix tf compile

--- dpl/run.sh
+++ dpl/run.sh
@@ -95,28 +95,30 @@ function compile_tensorflow(){
   local arch=$2 #x86_64
   echo "Start compile tensorflow: $target $arch"
 
+  OPTIONS="--verbose_failures -s -c opt --copt=-mavx --copt=-mfpmath=both --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2"
+  pushd ${MAIN_ROOT}/tools/tensorflow
+
   if [ ${target} == 'linux' ] && [ ${arch} == 'x86_64' ];then
+    ${BAZEL} build ${OPTIONS} //tensorflow:libtensorflow_cc.so || exit 1
+    echo "Compile tensorflow cpu successfully."
+  elif [ ${target} == 'linux' ] && [ ${arch} == 'gpu' ];then
     pushd ${MAIN_ROOT}/tools/tensorflow
-    ${BAZEL} build -c opt --verbose_failures //tensorflow:libtensorflow_cc.so || exit 1
-	
-    pushd bazel-bin/tensorflow
-    #if [ -L libtensorflow_cc.so.1 ]; then
-    #    unlink libtensorflow_cc.so.1
-    #fi
-    #ln -s libtensorflow_cc.so.1 libtensorflow_cc.so
-    if [ ! -L libtensorflow_framework.so ];then
-      ln -s libtensorflow_framework.so.2 libtensorflow_framework.so
-    fi
-    cp *.so* ${MAIN_ROOT}/dpl/lib/tensorflow/
-    popd
-    popd
-
-    echo "Compile tensorflow successfully."
-
+    ${BAZEL} build ${OPTIONS} --config=cuda -k //tensorflow:libtensorflow_cc.so || exit 1
+    echo "Compile tensorflow gpu successfully."
   else
     echo "Not support: $target $arch"
     exit 1
   fi
+
+  pushd bazel-bin/tensorflow
+  if [ ! -L libtensorflow_framework.so ];then
+    ln -s libtensorflow_framework.so.2 libtensorflow_framework.so
+  fi
+  cp *.so* ${MAIN_ROOT}/dpl/lib/tensorflow/
+  popd
+
+
+  popd
   echo
 }
 
