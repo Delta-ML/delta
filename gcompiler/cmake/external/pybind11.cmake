2fcab3468000506d66b015f6a8b8771853cc33b5
Hui Zhang <zhtclz@foxmail.com>
2020-08-07 23:59:57 +0800

    add pybind11 download

---
+++ gcompiler/cmake/external/pybind11.cmake
@@ -0,0 +1,12 @@
+include(FetchContent)
+
+set(PYBIND11_DIR ${DELTA_INFER_ROOT}/third_party/pybind11)
+FetchContent_Declare(
+  pybind11 
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG        master 
+  INSTALL_DIR   ${PYBIND11_DIR}
+  SOURCE_DI R   ${PYBIND11_DIR}
+)
+
+FetchContent_MakeAvailable(pybind11)
