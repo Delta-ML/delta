18b8deef2a59396747e9c59f360340b200f47606
Hui Zhang <zhtclz@foxmail.com>
2020-08-08 09:40:16 +0800

    fix

--- gcompiler/cmake/external/pybind11.cmake
+++ gcompiler/cmake/external/pybind11.cmake
@@ -3,10 +3,11 @@ include(FetchContent)
 set(PYBIND11_DIR ${DELTA_INFER_ROOT}/third_party/pybind11)
 FetchContent_Declare(
   pybind11 
-  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_REPOSITORY https://github.com/pybind/pybind11.git
   GIT_TAG        master 
   DOWNLOAD_DIR  ${PYBIND11_DIR}
   SOURCE_DIR    ${PYBIND11_DIR}
 )
 
+delta_msg(INFO STR "pybind11: ${PYBIND11_DIR}")
 FetchContent_MakeAvailable(pybind11)
